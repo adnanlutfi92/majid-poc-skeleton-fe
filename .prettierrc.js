7ee1d8c829aad08af6dcb7ee1dce6767ae3762d3
majid <bimantarasc@gmail.com>
2024-09-19 09:53:38 +0700

    add prettier & config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  // htmlWhitespaceSensitivity: 'css',
+  // jsxSingleQuote: true,
+  semi: false, // titik koma (;)
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Home from '../page'
- 
+
 test('Home', () => {
   render(<Home />)
   expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
