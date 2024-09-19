baab88beb22653c46db262ee7ce5a60b8f95c7dd
majid <bimantarasc@gmail.com>
2024-09-19 13:31:43 +0700

    update config prettier & test create new page contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Home</h1>
+      <div className="flex gap-2 text-blue-500">
+        <Link href={'/'}>
+          <h1>Home</h1>
+        </Link>
+        <Link href={'/contact'}>Contact</Link>
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
