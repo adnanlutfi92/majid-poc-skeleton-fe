a47e157f3002a1a3d401ab32c502f68949eda2eb
majid <bimantarasc@gmail.com>
2024-09-20 08:25:35 +0700

    test using shadcn/ui

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,10 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@radix-ui/react-accordion'
 import Link from 'next/link'
 import React from 'react'
 
 const ContactPage: React.FC = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-3 min-h-screen justify-center">
       <h1>Contact Us</h1>
       <form>
         <div>
@@ -24,6 +25,27 @@ const ContactPage: React.FC = () => {
       <Link href="/" className="underline text-blue-500 hover:text-blue-100">
         Home
       </Link>
+
+      <div className="flex flex-col min-w-96">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem className="border-b" value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   )
 }
